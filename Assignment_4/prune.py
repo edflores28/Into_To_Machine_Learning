4505c601c9929e3376ca7b4a0a2103b35949b8f1
Edwin Flores <edflores28@gmail.com>
2018-03-31 11:53:40 -0400

    Cleaned up the file and added comments

--- Assignment_4/prune.py
+++ Assignment_4/prune.py
@@ -1,50 +1,97 @@
 import tree_utils
 import copy
+import node
+
+'''
+This class performs reduced error pruning on the decision tree
+'''
 
 
 class Prune:
     def __init__(self, root, prune_set):
+        '''
+        Constructor for the pruning algorithm. Takes in the
+        root node and the pruning set
+        '''
         self.root = root
         self.prune_set = prune_set
 
-    def __get_leaves(self, node, labels):
-        branches = node.get_branches()
+    def __get_leaves(self, thenode, labels):
+        '''
+        This method obtains all the labels of the leaves
+        given thenode
+        '''
+        # Obtain the branches and iterate through them
+        branches = thenode.get_branches()
         for key in branches:
+            # If the node is a leaf get the label and
+            # add it to the labels lst
             if branches[key].get_leaf():
                 label = branches[key].get_label()
                 if label not in labels:
                     labels[label] = 1
                 else:
                     labels[label] += 1
+            # Otherwise do recursion
             else:
                 self.__get_leaves(branches[key], labels)
         return labels
 
-    def __determine_label(self, node):
+    def __determine_label(self, thenode):
+        '''
+        This method obtains the label of the node
+        and returns the most occuring label
+        '''
         labels = {}
-        labels = self.__get_leaves(node, labels)
+        labels = self.__get_leaves(thenode, labels)
         return max(labels.items(), key=lambda x: x[1])[0]
 
-    def __get_pruned_trees(self, root):
-        branches = root.get_branches()
+    def __get_pruned_trees(self, is_root, thenode):
+        '''
+        This method iterates through the entire tree and
+        creates a new tree at each node, with the node
+        turned into a leaf with the most common label
+        '''
+        subtree_list = []
+        # Get the branches at the root node
+        if is_root:
+            branches = self.root.get_branches()
+        # Otherwise get the branches at thenode
+        else:
+            branches = thenode.get_branches()
+        # Iterate through the branches
         for key in branches:
-            if branches[key].get_leaf() is False and branches[key].get_pruned() is False:
+            # When the node is an interior node
+            if branches[key].get_leaf() is False:
+                # Create a copy of the node
+                node_copy = branches[key]
+                # Determine the best label of the node
                 label = self.__determine_label(branches[key])
-                branches[key].set_label(label)
-                branches[key].set_pruned(True)
-                return
+                # Create a new node and set the label
+                new_node = node.Node()
+                new_node.set_label(label)
+                # Replace the node with the new node, this
+                # prunes the tree.
+                branches[key] = new_node
+                # Copy the whole tree and add it to the list
+                subtree_list.append(copy.deepcopy(self.root))
+                # Replace the node with it's original self
+                branches[key] = node_copy
+                # Continue traversing the tree
+                subtree_list += self.__get_pruned_trees(False, branches[key])
+        return subtree_list
 
-    def reduced_error_prune(self, root):
-        performance = {}
-        # Determine the accuracy of the original tree
-        #accuracy = tree_utils.predict_accuracy(self.prune_set, self.root)
-        # Add the tree to the dictionary
-        #performance[accuracy] = self.root
-        # Obtain the branches of the root node
-        branches = root.get_branches()
-        # Only iterate through the interior nodes
-
-        self.__get_pruned_trees(root)
-        branches = root.get_branches()
-        print(branches['left'].get_pruned())
-        return performance
+    def reduced_error_prune(self):
+        '''
+        This method starts the reduced error pruning algorithm
+        '''
+        performance = []
+        # Obtain all the trees
+        sublist = self.__get_pruned_trees(self.root, True)
+        # Iterate through each tree and determine the accuracy with
+        # the pruning set.
+        for entry in sublist:
+            accuracy = tree_utils.predict_accuracy(self.prune_set, entry)
+            performance.append(accuracy)
+        # Return the maximum performance
+        return max(performance)
