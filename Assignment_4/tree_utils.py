9a47cc72f639a038ba5643176274d4dd27ba49ec
Edwin Flores <edflores28@gmail.com>
2018-03-31 10:49:28 -0400

    Cleaned up the file and added comments

--- Assignment_4/tree_utils.py
+++ Assignment_4/tree_utils.py
@@ -1,4 +1,15 @@
+import random
+
+'''
+This package provides utilities for decision trees
+'''
+
+
 def print_node(node):
+    '''
+    This method iterates through the node and
+    prints out information.
+    '''
     if node is None:
         return
     if node.get_leaf():
@@ -12,35 +23,75 @@ def print_node(node):
             print_node(branches[key])
 
 
-def determine_label(test, node):
-    # If the node node is a leaf node return the label
-    if node.get_leaf() is True:
-        return node.get_label()
+def determine_label(test, root):
+    '''
+    This method takes in the root of the tree and
+    a test entry and traverses the tree until a
+    label is found
+    '''
+    # If the root root is a leaf root return the label
+    if root.get_leaf() is True:
+        return root.get_label()
+    # Otherwise continue iterating through the tree
     else:
-        index = node.get_feature_index()
+        index = root.get_feature_index()
         value = test[index]
-        branches = node.get_branches()
-        threshold = node.get_threshold()
+        branches = root.get_branches()
+        threshold = root.get_threshold()
+        # If the branches are catergorical then use the value
+        # as the key for branches
         if threshold is None:
+            # On misclassification return 0.0
             try:
                 return determine_label(test, branches[value])
             except:
-                print("except")
                 return 0.0
+        # Otherwise use the threshold
         else:
-            if value < threshold:
-                return determine_label(test, branches['left'])
+            # When the value is less than or equal to the threshold
+            # use the left branch for recursion
+            if value <= threshold:
+                try:
+                    return determine_label(test, branches['left'])
+                except:
+                    return 0.0
+            # Otherwise use the right branch for recursion
             else:
-                return determine_label(test, branches['right'])
+                # On misclassification return 0.0
+                try:
+                    return determine_label(test, branches['right'])
+                except:
+                    return 0.0
 
 
-def predict_accuracy(test, node):
+def predict_accuracy(test, root):
+    '''
+    This method takes the testing set and the
+    root node of the tree. Each entry is validated
+    based on the prediction and the % correct is
+    returned
+    '''
     correct = 0
     incorrect = 0
     for entry in range(len(test)):
-        x = determine_label(test[entry], node)
+        x = determine_label(test[entry], root)
         if x == test[entry][-1]:
             correct += 1
         else:
-            incorrect +=1
+            incorrect += 1
     return(correct*100)/(incorrect+correct)
+
+
+def get_pruning_set(train):
+    '''
+    This method removes 10% of the training set in order
+    to create a pruning set
+    '''
+    prune_length = int(round(len(train)*.10, 0))
+    prune_set = []
+    # Pop random index values from the training set
+    # and add them to the pruning set.
+    while len(prune_set) < prune_length:
+        index = random.sample(range(len(train)), 1)
+        prune_set.append(train.pop(index[0]))
+    return prune_set, train
